eb636265b2be2c2543bf6372e59ffe8278ba86e0
Dessa19 <dessalew25@gmail.com>
2024-10-07 12:50:06 -0700

    Testing Assignment

---
+++ src/Testing/AlgorithmTest.java
@@ -0,0 +1,18 @@
+package Testing;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+class AlgorithmTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+    @org.junit.jupiter.api.Test
+    static char reverseArr() {
+        char test1 = AlgorithmTest.reverseArr();
+        assertArrayEquals();
+        return test1;
+    }
+
+    private static void assertArrayEquals() {
+    }
+
+}
